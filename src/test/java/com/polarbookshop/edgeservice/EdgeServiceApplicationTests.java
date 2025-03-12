ed1123ce21f37fc67d2540b2985198b3cbda7e1e
kkamdung <kkamdung@empal.com>
2025-03-12 16:27:47 +0900

    테스트 케이스 오류 임시 수정

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -14,6 +16,9 @@ import org.testcontainers.utility.DockerImageName;
 class EdgeServiceApplicationTests {
 
     private static final int REDIS_PORT = 6379;
+
+    @MockitoBean
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
 
     @Container
     static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
