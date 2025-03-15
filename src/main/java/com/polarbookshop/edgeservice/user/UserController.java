ecea6fe8b38e06d51dfae5e9a14e75e31389dd21
kkamdung <kkamdung@empal.com>
2025-03-15 15:01:15 +0900

    로그 메시지 추가

--- src/main/java/com/polarbookshop/edgeservice/user/UserController.java
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -1,16 +1,19 @@
 package com.polarbookshop.edgeservice.user;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RestController
 public class UserController {
 
     @GetMapping("/user")
     public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        log.info("Fetching information about the currently authenticated user");
         User user = new User(oidcUser.getPreferredUsername(),
                 oidcUser.getGivenName(),
                 oidcUser.getFamilyName(),
