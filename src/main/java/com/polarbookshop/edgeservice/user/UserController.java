a7aa724ee31f0103492bb1c6090737ac25d5112b
kkamdung <kkamdung@empal.com>
2025-04-05 14:47:37 +0900

    URL 수정

--- src/main/java/com/polarbookshop/edgeservice/user/UserController.java
+++ src/main/java/com/polarbookshop/edgeservice/user/UserController.java
@@ -1,8 +1,6 @@
 package com.polarbookshop.edgeservice.user;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.ReactiveSecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,21 +11,8 @@ import java.util.List;
 @RestController
 public class UserController {
 
-    @GetMapping("/user/code")
-    public Mono<User> getUserByCode() {
-        return ReactiveSecurityContextHolder.getContext()
-                .map(SecurityContext::getAuthentication)
-                .map(authentication -> {
-                    OidcUser principal = (OidcUser) authentication.getPrincipal();
-                    return new User(principal.getPreferredUsername(),
-                            principal.getGivenName(),
-                            principal.getFamilyName(),
-                            List.of("employee", "customer"));
-                });
-    }
-
-    @GetMapping("/user/annotation")
-    public Mono<User> getUserByAnnotation(@AuthenticationPrincipal OidcUser oidcUser) {
+    @GetMapping("/user")
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
         User user = new User(oidcUser.getPreferredUsername(),
                 oidcUser.getGivenName(),
                 oidcUser.getFamilyName(),
@@ -35,5 +20,18 @@ public class UserController {
 
         return Mono.just(user);
     }
+
+//    @GetMapping("/user/code")
+//    public Mono<User> getUserByCode() {
+//        return ReactiveSecurityContextHolder.getContext()
+//                .map(SecurityContext::getAuthentication)
+//                .map(authentication -> {
+//                    OidcUser principal = (OidcUser) authentication.getPrincipal();
+//                    return new User(principal.getPreferredUsername(),
+//                            principal.getGivenName(),
+//                            principal.getFamilyName(),
+//                            List.of("employee", "customer"));
+//                });
+//    }
 
 }
