cfe05d883c97af694e8177ebedb79fb0b946e414
kkamdung <kkamdung@empal.com>
2025-04-07 19:43:00 +0900

    액추에이터, 상태 프로브 설정

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
 
         return http
                 .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/actuator/**").permitAll()
                         .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
                         .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
                         .anyExchange().authenticated()
