076907f0a611f35f0e28f5e656e38e073d05c11d
kkamdung <kkamdung@empal.com>
2026-08-18 20:03:18 +0900

    키클록 클라이언트 설정

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.polarbookshop.edgeservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
@@ -14,7 +16,7 @@ public class SecurityConfig {
         return http
                 .authorizeExchange(exchange ->
                         exchange.anyExchange().authenticated())
-                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
 
